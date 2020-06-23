5511aca15ab4570202c5fc005dbfb87f6ea770e5
cansuoztrk <39691746+cansuoztrk@users.noreply.github.com>
2020-06-23 23:46:30 +0300

    Create jail-log.js

---
+++ komutlar/jail-log.js
@@ -0,0 +1,38 @@
+const Discord = require('discord.js')
+const db = require('quick.db');
+const ayarlar = require('../ayarlar.json')
+
+exports.run = async(client, message, args) => {
+if (!message.member.hasPermission('MANAGE_GUILD')) return message.reply(`**${ayarlar.prefix}jail-yetkilisi ayarla/sıfırla** isimli komutu kullanabilmek için \`SUNUCUYU YÖNET\` yetkisine sahip olman gerekiyor.`)
+if (!args[0]) return message.reply(`Sistemi kullanabilmek için, +jail-yetkilisi ayarla/sıfırla @rol yazmalısın.\nDetaylı bilgi için: +yardım sustur-kanal`)
+   
+  
+  if (args[0] == 'ayarla') {
+  
+  let kanal = message.mentions.channels.first() || message.guild.channels.find(c => c.name === args[1].join('-'))
+  if (!kanal) return message.channel.send(`Bir kanal etiketle.`)
+  
+  db.set(`jailkanal_${message.guild.id}`, kanal.id)
+  message.channel.send(`Jail logunun tutulacağı kanal ${kanal} olarak ayarlandı.`)
+  } 
+  
+
+  if (args[0] == 'sıfırla') {
+    db.delete(`jailkanal_${message.guild.id}`)
+    message.channel.send(`Jail logunun tutulduğu kanal başarıyla sıfırlandı.`)
+  }
+  
+  
+};
+exports.conf = {
+ enabled: true,
+ guildOnly: false,
+ aliases: ['jailkanal',"jaillog"],
+ permLevel: 0
+};
+
+exports.help = {
+ name: 'jail-kanal',
+ description: 'Birisi jaile atılınca hangi kanala mesaj atılacağını ayarlarsınız.',
+ usage: 'jail-kanal ayarla/sıfırla #kanal',
+};
