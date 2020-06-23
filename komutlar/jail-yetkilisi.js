1ec39774a55c6bf25ae93a0845072e85b9b6bb60
cansuoztrk <39691746+cansuoztrk@users.noreply.github.com>
2020-06-23 23:46:54 +0300

    Create jail-yetkilisi.js

---
+++ komutlar/jail-yetkilisi.js
@@ -0,0 +1,38 @@
+const Discord = require('discord.js')
+const db = require('quick.db');
+const ayarlar = require('../ayarlar.json')
+
+exports.run = async(client, message, args) => {
+if (!message.member.hasPermission('MANAGE_GUILD')) return message.reply(`**${ayarlar.prefix}jail-yetkilisi ayarla/sıfırla** isimli komutu kullanabilmek için \`SUNUCUYU YÖNET\` yetkisine sahip olman gerekiyor.`)
+if (!args[0]) return message.reply(`Sistemi kullanabilmek için, +jail-yetkilisi ayarla/sıfırla @rol yazmalısın.\nDetaylı bilgi için: +yardım sustur-kanal`)
+   
+  
+  if (args[0] == 'ayarla') {
+  
+  let yetkilirol = message.mentions.roles.first() || message.guild.roles.find(c => c.name === args[1].join(' '))
+  if (!yetkilirol) return message.channel.send(`Bir rol etiketle.`)
+  
+  db.set(`jailyetkilisi_${message.guild.id}`, yetkilirol.id)
+  message.channel.send(`Jail yetkilisi ${yetkilirol} olarak ayarlandı.`)
+  } 
+  
+
+  if (args[0] == 'sıfırla') {
+    db.delete(`jailyetkilisi_${message.guild.id}`)
+    message.channel.send(`Jail yetkilisi başarıyla sıfırlandı.`)
+  }
+  
+  
+};
+exports.conf = {
+ enabled: true,
+ guildOnly: false,
+ aliases: ['jailyetkilisi'],
+ permLevel: 0
+};
+
+exports.help = {
+ name: 'jail-yetkilisi',
+ description: 'Hangi role sahip kişilerin jaile atabileceğini ayarlarsınız.',
+ usage: 'jail-yetkilisi ayarla/sıfırla @rol',
+};
